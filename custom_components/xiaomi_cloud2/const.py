35326da73ed5286c1cfe907d576d118302394ea4
dss16694 <dss16694@163.com>
2020-10-19 14:31:04 +0800

    change to cloud2 step2

--- custom_components/xiaomi_cloud2/const.py
+++ custom_components/xiaomi_cloud2/const.py
@@ -10,7 +10,7 @@ LastEditTime  : 2020-09-22 22:49:23
 
 """Const file for Xiaomi Cloud."""
 CONF_WAKE_ON_START = "enable_wake_on_start"
-DOMAIN = "xiaomi_cloud"
+DOMAIN = "xiaomi_cloud2"
 COORDINATOR = "coordinator"
 DATA_LISTENER = "listener"
 UNDO_UPDATE_LISTENER = "undo_update_listener"
